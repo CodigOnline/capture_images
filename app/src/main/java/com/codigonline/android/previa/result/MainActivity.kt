a3239cef8a8a9d0cdc11c1df6862e44c1f90c5a9
Alvaro Ortega Marmol <alortegama@outlook.com>
2022-04-22 12:44:30 +0200

    Capture image with default camera
    - Create layout with ConstraintLayout
    - Open camera app and take result

---
+++ app/src/main/java/com/codigonline/android/previa/result/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.codigonline.android.previa.result
+
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.codigonline.android.previa.result.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openCamera.setOnClickListener {
+
+            openCamera.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        }
+
+    }
+    val openCamera =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data!!
+                val bitmap = data.extras!!.get("data") as Bitmap
+                binding.img.setImageBitmap(bitmap)
+
+            }
+        }
+
+
+
+}
